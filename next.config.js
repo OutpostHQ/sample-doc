f17487aeb55643bb2ad55f1514830c974e85b513
ajeya <ajeyabhat.off@gmail.com>
2022-07-14 14:10:56 +0530

    added next-compose-plugins dep and changes config for i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const withMarkdoc = require('@markdoc/next.js');
-
-module.exports =
-  withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
-  });
+const withMarkdoc = require('@markdoc/next.js')
+const withPlugins = require('next-compose-plugins')
+const I18N = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+}
+module.exports = withPlugins(
+  [
+    withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
+      pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
+    }),
+  ],
+  I18N
+)
