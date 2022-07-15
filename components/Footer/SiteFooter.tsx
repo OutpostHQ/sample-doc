99affaa221ad9045103d2be10d50845b6bcd6fce
Rrhul0 <rr025765@gmail.com>
2022-07-15 16:46:13 +0530

    fix for site footer responsiveness

--- components/Footer/SiteFooter.tsx
+++ components/Footer/SiteFooter.tsx
@@ -1,17 +1,32 @@
-import { Flex, Text } from '@cube-dev/ui-kit'
+import { CopyrightCircleOutlined } from '@ant-design/icons'
+import { BreakpointsProvider, Flex, Text } from '@cube-dev/ui-kit'
 
 export const SiteFooter = () => {
   return (
-    <Flex
-      justifyContent="space-between"
-      padding="15px 30px"
-      alignItems="center"
-      height="100%"
-    >
-      <Text>
-        Built by <Text weight={'bold'}>Outpost</Text>
-      </Text>
-      <Text>Copyright 2022 Outpost Innovations, Inc. / Trademark Policy</Text>
-    </Flex>
+    <BreakpointsProvider value={[1200, 900]}>
+      <Flex
+        justifyContent="space-between"
+        margin="auto"
+        padding="25px 5%"
+        alignItems="center"
+        height="144px"
+        flow={['row', , 'column']}
+      >
+        <Text weight={'500'}>
+          Built by <Text weight={'700'}>Outpost</Text>
+        </Text>
+        <Text display={['initial', , 'none']} weight="500">
+          Copyright <CopyrightCircleOutlined /> 2022 Outpost Innovations, Inc. /
+          <Text> Trademark Policy</Text>
+        </Text>
+        <Text display={['none', , 'initial']}>
+          Copyright <CopyrightCircleOutlined /> 2022 Outpost. All rights
+          reserved.
+        </Text>
+        <Text display={['none', , 'initial']} weight="500">
+          Trademark Policy
+        </Text>
+      </Flex>
+    </BreakpointsProvider>
   )
 }
