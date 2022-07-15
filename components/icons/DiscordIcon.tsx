2afebf93c34c8cef0248e82603f992bac71ac88e
Rrhul0 <rr025765@gmail.com>
2022-07-15 19:32:49 +0530

    little fix for icons size

--- components/icons/DiscordIcon.tsx
+++ components/icons/DiscordIcon.tsx
@@ -1,8 +1,8 @@
 export function DiscordIcon() {
   return (
     <svg
-      width="24"
-      height="19"
+      width="20"
+      height="20"
       viewBox="0 0 24 19"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- components/icons/GithubIcon.tsx
+++ components/icons/GithubIcon.tsx
@@ -2,7 +2,7 @@ export function GithubIcon() {
   return (
     <svg
       width="20"
-      height="21"
+      height="20"
       viewBox="0 0 20 21"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
