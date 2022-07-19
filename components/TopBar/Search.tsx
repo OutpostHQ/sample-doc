5505981f00495b786a9c474f48c6b77b71a92baf
Rrhul0 <rr025765@gmail.com>
2022-07-19 13:47:43 +0530

    fixed search algolia, using test keys of algolia

--- components/TopBar/Search.tsx
+++ components/TopBar/Search.tsx
@@ -3,10 +3,11 @@ import { DocSearch } from '@docsearch/react'
 export default function Search() {
   return (
     <DocSearch
-      appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}
-      apiKey={process.env.NEXT_PUBLIC_ALGOLIA_API_KEY}
-      indexName="markdoc"
-      placeholder="Search..."
+      appId="R2IYF7ETH7" //{process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}
+      apiKey="599cec31baffa4868cae4e79f180729b" //{process.env.NEXT_PUBLIC_ALGOLIA_API_KEY}
+      indexName="docsearch" //"markdoc"
+      placeholder="Search The Docs"
+      disableUserPersonalization
     />
   )
 }
